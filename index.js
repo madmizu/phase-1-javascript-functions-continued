ce5bf11635e6e1d548e58ce62900ac2a1ba5de76
Madison Shimizu <mhshimizu@outlook.com>
2021-11-02 15:40:06 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun (activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+    };
+
+function mondayWork (action = "go to the office") {
+    return (`This Monday, I will ${action}.`);
+    };
+
+    function wrapAdjective(highlight = "*"){
+    
+        return function(adjective) {
+            return `You are ${highlight}${adjective}${highlight}!`
+        }
+    };
